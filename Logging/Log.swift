64fcd7338847f65fc8948ab3edb8a51b0fcb0699
Vitalii Yevtushenko <vitalii@rollncode.com>
2016-08-06 18:02:02 +0300

    init

---
+++ Logging/Log.swift
@@ -0,0 +1,50 @@
+//
+//  Log.swift
+//  Paragraph
+//
+//  Created by Vitalii Yevtushenko on 8/6/16.
+//  Copyright © 2016 Roll'n'Code. All rights reserved.
+//
+
+import Foundation
+
+public class Log {
+
+    static var enabled = false
+    static var level = LogLevel.verbose
+
+    private static var startTime: NSDate?
+    private static var lastTime: NSDate?
+
+    private static func _print(text: String, level messageLevel: LogLevel) {
+        if !enabled || messageLevel.rawValue < level.rawValue {
+            return
+        }
+
+        let time = NSDate()
+
+        if let theLastTime = lastTime {
+            let deltaTimeStr = String(format: "%.03f", time.timeIntervalSinceDate(theLastTime) * 1000.0)
+            let absoluteTimeStr = String(format: "%.03f", time.timeIntervalSinceDate(startTime!))
+            print("\(absoluteTimeStr) +\(deltaTimeStr) \(messageLevel.prefix): \(text)")
+        } else {
+            print("0.000 +0.000 \(messageLevel.prefix): \(text)")
+            startTime = time
+        }
+
+        lastTime = time
+    }
+
+    static func verbose(text: String) {
+        _print(text, level: .verbose)
+    }
+
+    static func warning(text: String) {
+        _print(text, level: .warning)
+    }
+
+    static func error(text: String) {
+        _print(text, level: .error)
+    }
+
+}
---
+++ Logging/LogLevel.swift
@@ -0,0 +1,21 @@
+//
+//  LogLevel.swift
+//  Paragraph
+//
+//  Created by Vitalii Yevtushenko on 8/6/16.
+//  Copyright © 2016 Roll'n'Code. All rights reserved.
+//
+
+import Foundation
+
+public enum LogLevel: Int {
+    case verbose
+    case warning
+    case error
+
+    var prefix: String {
+        get {
+            return "\(self)"
+        }
+    }
+}
